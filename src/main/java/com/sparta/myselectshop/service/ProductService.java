db2f05686ee8c53f03813f848006722a2a3903b8
leeseowoo <leeseowoo.kr@gmail.com>
2025-02-05 19:23:36 +0900

    feat: price update scheduler

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -4,6 +4,7 @@ import com.sparta.myselectshop.dto.ProductMyPriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.naver.dto.ItemDto;
 import com.sparta.myselectshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -48,5 +49,14 @@ public class ProductService {
         }
 
         return productResponseDtoList;
+    }
+
+    @Transactional
+    public void updateBySearch(Long id, ItemDto itemDto) {
+        Product product = productRepository.findById(id).orElseThrow(
+                () -> new NullPointerException("해당 상품을 찾을 수 없습니다.")
+        );
+
+        product.updateByItemDto(itemDto);
     }
 }
